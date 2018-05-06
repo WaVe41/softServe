39470aefebf2d2686cbd5c23bd6b4bcec8de1767
WaVe41 <farellmorrison@gmail.com>
2018-05-06 23:51:28 +0300

    up

--- spec/ClassesTask.js
+++ spec/ClassesTask.js
@@ -9,34 +9,75 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var Employee = /** @class */ (function () {
-    function Employee(name, secondName, salary, experiance, manager) {
+    function Employee(name, secondName, salary, experience) {
         this.name = name;
         this.secondName = secondName;
         this.salary = salary;
-        this.experiance = experiance;
-        this.manager = manager;
+        this.experience = experience;
     }
     return Employee;
 }());
 var Developer = /** @class */ (function (_super) {
     __extends(Developer, _super);
-    function Developer() {
-        return _super !== null && _super.apply(this, arguments) || this;
+    function Developer(name, secondName, salary, experience, manager) {
+        var _this = _super.call(this, name, secondName, salary, experience) || this;
+        _this.manager = manager;
+        return _this;
     }
+    Developer.prototype.giveSalary = function (salary) {
+        this.salary = salary;
+        if (this.experience > 2 && this.experience <= 5)
+            this.salary += 200;
+        else if (this.experience > 5)
+            this.salary = this.salary * 1.2 + 500;
+    };
     return Developer;
 }(Employee));
 var Designer = /** @class */ (function (_super) {
     __extends(Designer, _super);
-    function Designer() {
-        return _super !== null && _super.apply(this, arguments) || this;
+    function Designer(name, secondName, salary, experience, manager, effectivnessCoef) {
+        var _this = _super.call(this, name, secondName, salary, experience) || this;
+        _this.manager = manager;
+        _this.effectivnessCoefficient = effectivnessCoef;
+        return _this;
     }
+    Designer.prototype.giveSalary = function (salary) {
+        this.salary = salary;
+        this.salary *= this.effectivnessCoefficient;
+    };
     return Designer;
 }(Employee));
 var Manager = /** @class */ (function (_super) {
     __extends(Manager, _super);
-    function Manager() {
-        return _super !== null && _super.apply(this, arguments) || this;
+    function Manager(name, secondName, salary, experience, team) {
+        var _this = _super.call(this, name, secondName, salary, experience) || this;
+        _this.team = team;
+        return _this;
     }
+    Manager.prototype.giveSalary = function (salary) {
+        this.salary = salary;
+        var devs = this.team.filter(function (val) { return val instanceof Developer; });
+        if (2 * devs.length > this.team.length)
+            this.salary *= 1.1;
+        if (this.team.length > 5 && this.team.length <= 10)
+            this.salary += 200;
+        else if (this.team.length > 10)
+            this.salary += 300;
+    };
     return Manager;
 }(Employee));
+var Departament = /** @class */ (function () {
+    function Departament() {
+    }
+    Departament.prototype.giveSalary = function (name, secondName, salary) {
+        var emp;
+        this.listOfManagers.forEach(function (val) {
+            emp = val.team.filter(function (e) { return e.name === name && e.secondName === secondName; })[0];
+        });
+        if (emp)
+            emp.giveSalary(salary);
+        console.log(name + ", " + secondName + ": got salary" + salary);
+    };
+    return Departament;
+}());
 //# sourceMappingURL=ClassesTask.js.map
--- spec/ClassesTask.ts
+++ spec/ClassesTask.ts
@@ -1,26 +1,69 @@
-class Employee {
-    name:string;
-    secondName:string;
-    salary:number;
-    experiance:number;
-    manager:string;
-    constructor(name:string, secondName:string, salary:number, experiance:number, manager:string) {
+abstract class Employee {
+    name: string;
+    secondName: string;
+    salary: number;
+    experience: number;
+    manager: string;
+
+    protected constructor(name: string, secondName: string, salary: number, experience: number) {
         this.name = name;
         this.secondName = secondName;
         this.salary = salary;
-        this.experiance = experiance;
-        this.manager = manager;
+        this.experience = experience;
     }
+    abstract giveSalary(salary: number): void;
 }
 
 class Developer extends Employee {
+    constructor(name:string, secondName:string, salary:number, experience:number, manager:string) {
+        super(name, secondName, salary, experience);
+        this.manager = manager;
+}
 
+    giveSalary(salary: number): void {
+        this.salary = salary;
+        if (this.experience > 2 && this.experience <= 5) this.salary += 200;
+        else if (this.experience > 5)  this.salary = this.salary * 1.2 + 500;
+    }
 }
 
 class Designer extends Employee{
-    effectivnessCoefficient:boolean;
+    effectivnessCoefficient:number;
+    constructor(name: string, secondName: string, salary: number, experience: number, manager: string, effectivnessCoef:number) {
+        super(name, secondName, salary, experience);
+        this.manager = manager;
+        this.effectivnessCoefficient = effectivnessCoef;
+    }
+    giveSalary(salary: number): void {
+        this.salary = salary;
+        this.salary *= this.effectivnessCoefficient;
+    }
 }
 
 class Manager extends Employee {
+    team:(Developer|Designer)[];
+    constructor(name: string, secondName: string, salary: number, experience: number, team?:(Developer|Designer)[]) {
+        super(name, secondName, salary, experience);
+        this.team = team;
+    }
+    giveSalary(salary: number): void {
+        this.salary = salary;
+        let devs:Developer[] = this.team.filter(val =>  val instanceof Developer);
+        if (2 * devs.length > this.team.length) this.salary *= 1.1;
+        if (this.team.length > 5 && this.team.length <= 10) this.salary += 200;
+        else if (this.team.length > 10) this.salary += 300;
+    }
 
+}
+
+class Departament {
+    listOfManagers:Manager[];
+    giveSalary(name:string, secondName:string, salary:number):void {
+        let emp:(Developer|Designer);
+        this.listOfManagers.forEach(val => {
+           emp = val.team.filter(e => e.name === name && e.secondName === secondName)[0];
+        });
+        if (emp) emp.giveSalary(salary);
+        console.log(`${name}, ${secondName}: got salary${salary}`);
+}
 }
